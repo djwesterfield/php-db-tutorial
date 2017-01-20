c6943a17530f5098c3e38e2fe52d5407d417d18b
djwesterfield <dj.westerfield@gmail.com>
2017-01-20 11:37:13 +0800

    added connection to db

--- add.php
+++ add.php
@@ -9,6 +9,22 @@
     $details = mysql_real_escape_string($_POST['details']);
     $time = strftime("%X"); //time
     $date = strftime("%B %d, %Y"); //date
+    $decision = "no";
+   
+       mysql_connect("localhost","root","") or die(mysql_error()); //Connect to server
+       mysql_select_db("first_db") or die("Cannot connect to database"); //Conect to database
+       foreach($_POST['public'] in $each_check) //gets the data from the checkbox
+       {
+          if($each_check != null){ //checks if checkbox is checked
+             $decision = "yes"; // sets the value
+          }
+       }
 
-    Print "$time - $date - $details";
+       mysql_query("INSERT INTO list(details, date_posted, time_posted, public) VALUES ('$details','$date','$time','$decision')"); //SQL query
+       header("location:home.php");
+    }
+    else
+    {
+       header("location:home.php");
+    }
 ?>
